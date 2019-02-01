a80b2cd595aa3c1c54c14af8037a5cb86a8e9f29
caffeintegral <kazusaichinomiya2220@gmail.com>
2019-02-01 11:17:21 +0900

    section1 caffe

---
+++ src/components/WForm.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template>
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
+@Component({})
+export default class WForm extends Vue {
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/views/SkillPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <w-skill-page/>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
+import WSkillPage from '@/components/WSkillPage.vue';
+
+@Component({
+  components: {
+    WSkillPage,
+  },
+})
+export default class SkillPage extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
